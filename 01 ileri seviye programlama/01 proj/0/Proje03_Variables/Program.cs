eeb80c0634d7646181e7c9a9f6c9c681d7451991
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-12-08 10:06:14 +0300

    .

---
+++ 01 ileri seviye programlama/01 proj/0/Proje03_Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿// namespace Proje03_Variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       /* 
+       
+       nullable types 
+       bu tipler , bir değerin yok (null) olabilmesine izin veren veri türleridir.
+       Genellikle referans tipler için mümküün olan bir durumdur 
+       varsayılan olarak null değer içeremen value typler istenilirse null deper içerebilecek hale getirilebilinir ? ile 
+       
+
+        var anahtar kelimesi tür çıkarımı (type inference) sağlayan bir kelimedir. Derleyici , değişkenin türünü
+        atanan değerden bakarak otomatik olarak belirler. Derleme zamanında bu tür sabit bir şekilde belirlenmiş olur
+
+        - var kullanımı , türün açık bir şekilde belli oldugu durumlarda kullanılırsa daha sağlıklı olur. tür bir kez
+        belirlendikten sonra değiştirilemez.    
+
+        dynamic , derleme zamanında tür kontrolü yapmadan çalışma zamanında geçerek orada bu çıkarsamanın yapılmasını
+        sağlar . Bu yönüyle daha esnek bir yapı sunarken hatalara da daha açık bir tekniktir.
+
+
+        */
+    }
+}
