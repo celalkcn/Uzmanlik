a63e3122d9f67f2dfb434b5dd330627e1ab54f5c
celal uğur kocan <celal_53_kocan@hotmail.com>
2024-12-28 11:40:20 +0300

    week4

--- 22-12-2024/Proje14_EfCore/EfCoreManyToMany/EfCore.Business/Concrete/ProductService.cs
+++ 22-12-2024/Proje14_EfCore/EfCoreManyToMany/EfCore.Business/Concrete/ProductService.cs
@@ -24,36 +24,157 @@ public class ProductService : IProductService
             Properties = productCreateDto.Properties
         };
         await _productRepository.AddAsync(product);
-        product.ProductCategories = 
+        product.ProductCategories = productCreateDto
+                            .CategoryIds.Select(x => new ProductCategory
+                            {
+                                ProductId = product.Id,
+                                CategoryId = x
+                            }).ToList();
+        await _productRepository.UpdateAsync(product);
+        var productDto = new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Properties = product.Properties
+        };
+        return productDto;
     }
 
-    public Task DeleteAsync(int id)
+    public async Task DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        var product = await _productRepository.GetByIdAsync(id);
+        await _productRepository.DeleteAsync(product);
     }
 
-    public Task<ProductDto> GetByIdAsync(int id)
+    public async Task<ProductDto> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var product = await _productRepository.GetProductWithCategoriesAsync(id);
+        var productDto = new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            IsDeleted = product.IsDeleted,
+            Price = product.Price,
+            Properties = product.Properties,
+            Categories = product
+                    .ProductCategories
+                    .Select(x => new CategoryDto
+                    {
+                        Id = x.CategoryId,
+                        Name = x.Category.Name,
+                        Description = x.Category.Description
+                    })
+                    .ToList()
+        };
+        return productDto;
     }
 
-    public Task<IEnumerable<ProductDto>> GetProductsAsync()
+    public async Task<IEnumerable<ProductDto>> GetProductsAsync()
     {
-        throw new NotImplementedException();
+        var products = await _productRepository.GetProductsWithCategoriesAsync();
+        var productDtos = products
+                .Select(x => new ProductDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Properties = x.Properties,
+                    Price = x.Price,
+                    IsDeleted = x.IsDeleted,
+                    Categories = x
+                        .ProductCategories
+                        .Select(x => new CategoryDto
+                        {
+                            Id = x.CategoryId,
+                            Name = x.Category.Name,
+                            Description = x.Category.Description
+                        })
+                        .ToList()
+                }).ToList();
+        return productDtos;
     }
 
-    public Task<IEnumerable<ProductDto>> GetProductsAsync(bool isDeleted)
+    public async Task<IEnumerable<ProductDto>> GetProductsAsync(bool isDeleted)
     {
-        throw new NotImplementedException();
+        var products = await _productRepository.GetAllDeletedProductsAsync(isDeleted);
+        var productDtos = products
+                .Select(x => new ProductDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Properties = x.Properties,
+                    Price = x.Price,
+                    IsDeleted = x.IsDeleted,
+                    Categories = x
+                        .ProductCategories
+                        .Select(x => new CategoryDto
+                        {
+                            Id = x.CategoryId,
+                            Name = x.Category.Name,
+                            Description = x.Category.Description
+                        })
+                        .ToList()
+                }).ToList();
+        return productDtos;
     }
 
-    public Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId)
+    public async Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId)
     {
-        throw new NotImplementedException();
+        var products = await _productRepository.GetProductsByCategoryAsync(categoryId);
+        var productDtos = products
+                .Select(x => new ProductDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Properties = x.Properties,
+                    Price = x.Price,
+                    IsDeleted = x.IsDeleted,
+                    Categories = x
+                        .ProductCategories
+                        .Select(x => new CategoryDto
+                        {
+                            Id = x.CategoryId,
+                            Name = x.Category.Name,
+                            Description = x.Category.Description
+                        })
+                        .ToList()
+                }).ToList();
+        return productDtos;
     }
 
-    public Task<ProductDto> UpdateAsync(ProductUpdateDto productUpdateDto)
+    public async Task<ProductDto> UpdateAsync(ProductUpdateDto productUpdateDto)
     {
-        throw new NotImplementedException();
+        var product = await _productRepository.GetProductWithCategoriesAsync(productUpdateDto.Id);
+        product.ProductCategories.Clear();
+        await _productRepository.UpdateAsync(product);
+
+        product.Name = productUpdateDto.Name;
+        product.Properties = productUpdateDto.Properties;
+        product.Price = productUpdateDto.Price;
+        product.ProductCategories = productUpdateDto
+            .CategoryIds
+            .Select(x => new ProductCategory
+            {
+                ProductId = product.Id,
+                CategoryId = x
+            }).ToList();
+        await _productRepository.UpdateAsync(product);
+        var productDto = new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Properties = product.Properties,
+            Price = product.Price,
+            IsDeleted = product.IsDeleted,
+            Categories = product
+                .ProductCategories
+                .Select(x => new CategoryDto
+                {
+                    Id = x.CategoryId,
+                    Name = x.Category.Name,
+                    Description = x.Category.Description
+                }).ToList()
+        };
+        return productDto;
     }
 }
--- 22-12-2024/Proje14_EfCore/EfCoreManyToMany/EfCore.Shared/Dtos/CategoryUpdateDto.cs
+++ 22-12-2024/Proje14_EfCore/EfCoreManyToMany/EfCore.Shared/Dtos/CategoryUpdateDto.cs
@@ -8,4 +8,5 @@ public class CategoryUpdateDto
     public string? Name { get; set; }
     public bool IsDeleted { get; set; }
     public string? Description { get; set; }
+    public List<CategoryDto>? CategoryList { get; set; }
 }
